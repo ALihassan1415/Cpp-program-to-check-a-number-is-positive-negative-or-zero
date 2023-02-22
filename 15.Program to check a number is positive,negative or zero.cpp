b6fd7577ad0e2f3ff44fe9b128f08f86d756565e
Ali Hassan <125749746+ALihassan1415@users.noreply.github.com>
2023-02-22 09:45:07 +0500

    Add files via upload

---
+++ 15.Program to check a number is positive,negative or zero.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int num;
+	cout<<"Enter the number : ";
+	cin>>num;
+	if(num>0)
+	cout<<"Number is Positive ";
+	else if(num<0)
+	cout<<"Number is negative ";
+	else
+	cout<<"Number is zero";
+}
